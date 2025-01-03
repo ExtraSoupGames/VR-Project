76c052390b89c01524c744a7483db5b23e0ca5c4
Doomturtles <jonny.collins111@gmail.com>
2025-01-03 17:39:49 +0000

    added information box prefab

---
+++ Assets/Scripts/Map/TerrainFalloff.cs
@@ -0,0 +1,35 @@
+﻿﻿using UnityEngine;
+using System.Collections;
+
+public static class TerrainFalloff {
+
+    public static float[,] CreateFalloffMap(int dimension) {
+        float[,] falloffMap = new float[dimension, dimension];
+
+        for (int row = 0; row < dimension; row++) {
+            for (int col = 0; col < dimension; col++) {
+                float horizontal = row / (float)dimension * 2 - 1;
+                float vertical = col / (float)dimension * 2 - 1;
+
+                float maxAxis = Mathf.Max(Mathf.Abs(horizontal), Mathf.Abs(vertical));
+                falloffMap[row, col] = CalculateFalloff(maxAxis);
+            }
+        }
+
+        // Invert the falloff map values
+        for (int row = 0; row < dimension; row++) {
+            for (int col = 0; col < dimension; col++) {
+                falloffMap[row, col] = 1f - falloffMap[row, col]; // Invert the values
+            }
+        }
+
+        return falloffMap;
+    }
+
+    static float CalculateFalloff(float input) {
+        float alpha = 3f;
+        float beta = 2.2f;
+
+        return Mathf.Pow(input, alpha) / (Mathf.Pow(input, alpha) + Mathf.Pow(beta - beta * input, alpha));
+    }
+}
