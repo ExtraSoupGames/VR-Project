2ea5c2ac9873748605f0c5e54674ff4765c86e3e
Doomturtles <jonny.collins111@gmail.com>
2025-01-05 12:46:02 +0000

    added a sample airlock and airlock controller

--- Assets/Scripts/AirlockController.cs
+++ Assets/Scripts/AirlockController.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class AirlockController : MonoBehaviour
 {
     public GameObject outerDoor;
+    private int outerDoorClosedX;
+    private int outerDoorOpenX;
     public GameObject innerDoor;
+    private int innerDoorClosedX;
+    private int innerDoorOpenX;
+    private float movementSpeed;
     enum AirlockState
     {
         OpenInside,
@@ -16,12 +21,112 @@ public class AirlockController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        outerDoorClosedX = 0;
+        outerDoorOpenX = 5;
+        innerDoorClosedX = 0;
+        innerDoorOpenX = 5;
+        movementSpeed = 3f;
+        state = AirlockState.Closed;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        switch (state)
+        {
+            case AirlockState.OpenInside:
+                if(outerDoor.transform.localPosition.x == outerDoorClosedX)
+                {
+                    if(innerDoor.transform.localPosition.x != innerDoorOpenX)
+                    {
+                        OpenInnerDoor();
+                    }
+                    else
+                    {
+                        //done
+                    }
+                }
+                else
+                {
+                    CloseOuterDoor();
+                }
+                break;
+            case AirlockState.OpenOutside:
+                if(innerDoor.transform.localPosition.x == innerDoorClosedX)
+                {
+                    if(outerDoor.transform.localPosition.x != outerDoorOpenX)
+                    {
+                        OpenOuterDoor();
+                    }
+                    else
+                    {
+                        //done
+                    }
+                }
+                else
+                {
+                    CloseInnerDoor();
+                }
+                break;
+            case AirlockState.Closed:
+                if(innerDoor.transform.localPosition.x != innerDoorClosedX)
+                {
+                    CloseInnerDoor();
+                }
+                if(outerDoor.transform.localPosition.x != outerDoorClosedX)
+                {
+                    CloseOuterDoor();
+                }
+                break;
+        }
+    }
+    private void OpenInnerDoor()
+    {
+        float difference = innerDoorOpenX - innerDoor.transform.localPosition.x;
+        innerDoor.transform.localPosition += new Vector3(Mathf.Clamp(difference, -movementSpeed * Time.deltaTime, movementSpeed * Time.deltaTime), 0, 0);
+    }
+    private void CloseInnerDoor()
+    {
+        float difference = innerDoorClosedX - innerDoor.transform.localPosition.x;
+        innerDoor.transform.localPosition += new Vector3(Mathf.Clamp(difference, -movementSpeed * Time.deltaTime, movementSpeed * Time.deltaTime), 0, 0);
+    }
+    private void OpenOuterDoor()
+    {
+        float difference = outerDoorOpenX - outerDoor.transform.localPosition.x;
+        outerDoor.transform.localPosition += new Vector3(Mathf.Clamp(difference, -movementSpeed * Time.deltaTime, movementSpeed * Time.deltaTime), 0, 0);
+    }
+    private void CloseOuterDoor()
+    {
+        float difference = outerDoorClosedX - outerDoor.transform.localPosition.x;
+        outerDoor.transform.localPosition += new Vector3(Mathf.Clamp(difference, -movementSpeed * Time.deltaTime, movementSpeed * Time.deltaTime), 0, 0);
+
+    }
+    public void OpenInnerDoorPressed()
+    {
+        if(state == AirlockState.Closed)
+        {
+            state = AirlockState.OpenInside;
+        }
+    }
+    public void CloseInnerDoorPressed()
+    {
+        if(state == AirlockState.OpenInside)
+        {
+            state = AirlockState.Closed;
+        }
+    }
+    public void OpenOuterDoorPressed()
+    {
+        if(state == AirlockState.Closed)
+        {
+            state = AirlockState.OpenOutside;
+        }
+    }
+    public void CloseOuterDoorPressed() 
+    {
+        if(state == AirlockState.OpenOutside)
+        {
+            state = AirlockState.Closed;
+        }
     }
 }
